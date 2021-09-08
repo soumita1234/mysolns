d49d0aa8116bf3c27e3a61491e6f0aca8c5fe954
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-09-08 20:17:07 +0530

    Create Swap Without Third Variable.cpp

---
+++ Basic Interview/Swap Without Third Variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a=10;
+    int b=5;
+    a=a+b; //15
+    b=a-b; //15-5=10
+    a=a-b; //15-10=5
+    cout<<"A ="<<a;
+    cout<<endl;
+    cout<<"B ="<<b;
+    
+	return 0;
+}
